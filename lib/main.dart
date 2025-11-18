b0cc739909555a4d084b73fba8d0fb49e8eeed89
deanissa <deanissa604@gmail.com>
2025-11-18 17:25:21 +0700

    fix merge conflict

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,9 @@
+
+import 'package:antrean_poliklinik/features/auth/welcome_page.dart';
+import 'package:antrean_poliklinik/features/caller/controllers/caller_controller.dart';
+import 'package:antrean_poliklinik/features/kios/controllers/kios_controller.dart';
+import 'package:antrean_poliklinik/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -117,6 +123,25 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+
+   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+// This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Antrean Poliklinik',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        ),
+        home: const WelcomeScreen(),
     );
   }
 }
